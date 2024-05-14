75c685eecf64219466a5b7df2ba60d7dcfb8c882
Patryk Maciak <maciakpatryk32@gmail.com>
2024-05-14 10:03:19 +0200

    first app, carousel with images, left and right btn

---
+++ ImageFirstApp/app/src/main/java/com/example/imagefirstapp/Image.java
@@ -0,0 +1,44 @@
+package com.example.imagefirstapp;
+
+import java.util.ArrayList;
+
+public class Image {
+    private String name;
+    private String desc;
+    private int src;
+
+    public static ArrayList<Image> images = new ArrayList<Image>() {{
+        add(new Image("camera1", "sth with camera", R.drawable.ic_baseline_camera_alt_24));
+        add(new Image("camera2", "sth with camera", R.drawable.camera2));
+        add(new Image("camera3", "sth with camera", R.drawable.camera3));
+    }};
+
+    public Image(String name, String desc, int src) {
+        this.name = name;
+        this.desc = desc;
+        this.src = src;
+    }
+
+    public Image(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+        this.src = R.drawable.ic_launcher_foreground;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getSrc() {
+        return src;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.desc;
+    }
+}
--- ImageFirstApp/app/src/main/java/com/example/imagefirstapp/MainActivity.java
+++ ImageFirstApp/app/src/main/java/com/example/imagefirstapp/MainActivity.java
@@ -3,12 +3,65 @@ package com.example.imagefirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button previousBtn, nextBtn;
+    private EditText imageNameEditText, imageDescEditText;
+    private ImageView galleryImageView;
+
+    private ArrayList<Image> images = new ArrayList<Image>();
+    private int currentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.images = Image.images;
+
+        previousBtn = (Button) findViewById(R.id.previousImageBtn);
+        nextBtn = (Button) findViewById(R.id.nextImageBtn);
+        imageNameEditText = (EditText) findViewById(R.id.imageNameEditText);
+        imageDescEditText = (EditText) findViewById(R.id.imageDescEditText);
+        galleryImageView = (ImageView) findViewById(R.id.galleryImageView);
+
+        galleryImageView.setVisibility(View.VISIBLE);
+        galleryImageView.setImageResource(images.get(0).getSrc());
+
+        previousBtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        currentIndex--;
+
+                        if(currentIndex < 0)
+                            currentIndex = images.size()-1;
+
+                        galleryImageView.setImageResource(images.get(currentIndex).getSrc());
+                    }
+                }
+        );
+
+        nextBtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        currentIndex++;
+
+                        if(currentIndex == images.size())
+                            currentIndex = 0;
+
+                        galleryImageView.setImageResource(images.get(currentIndex).getSrc());
+                    }
+                }
+        );
     }
 }
