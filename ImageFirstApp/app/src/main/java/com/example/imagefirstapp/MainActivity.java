fbf4f074f64b9c37754e37d2c0ab97495576795b
Patryk Maciak <maciakpatryk32@gmail.com>
2024-05-14 10:53:01 +0200

    displaying dynamiclly images from array into grid layout, set on click method on each one images

--- ImageFirstApp/app/src/main/java/com/example/imagefirstapp/MainActivity.java
+++ ImageFirstApp/app/src/main/java/com/example/imagefirstapp/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -17,7 +20,12 @@ public class MainActivity extends AppCompatActivity {
     private EditText imageNameEditText, imageDescEditText;
     private ImageView galleryImageView;
 
+    private GridLayout galleryGridLayout;
+
     private ArrayList<Image> images = new ArrayList<Image>();
+    private ArrayList<Image> views = new ArrayList<Image>();
+//    private ImageView[] views = new ImageView[10];
+
     private int currentIndex = 0;
 
     @Override
@@ -26,12 +34,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.images = Image.images;
+        this.views = Image.images;
 
         previousBtn = (Button) findViewById(R.id.previousImageBtn);
         nextBtn = (Button) findViewById(R.id.nextImageBtn);
         imageNameEditText = (EditText) findViewById(R.id.imageNameEditText);
         imageDescEditText = (EditText) findViewById(R.id.imageDescEditText);
         galleryImageView = (ImageView) findViewById(R.id.galleryImageView);
+        galleryGridLayout = (GridLayout) findViewById(R.id.centerGridLayout);
+
+        for(int i=0; i<views.size(); i++)
+        {
+            ImageView image = new ImageView(this);
+            image.setLayoutParams(new GridLayout.LayoutParams());
+            image.setImageResource(images.get(i).getSrc());
+            galleryGridLayout.addView(image);
+
+            final int position = i;
+
+            image.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Toast.makeText(MainActivity.this,
+                                    "Name: " + images.get(position).getName() +
+                                    "\nDesc: " + images.get(position).getDesc(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            );
+        }
+
+//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.MATCH_PARENT,
+//                LinearLayout.LayoutParams.MATCH_PARENT);
+//        params.weight = 1;
+
+//        for(int i=0; i<10; i++)
+//        {
+//            ImageView image = new ImageView(this);
+////            image.setLayoutParams(new android.view.ViewGroup.LayoutParams(200,200));
+////            image.setMaxHeight(20);
+////            image.setMaxWidth(20);
+//            image.setLayoutParams(new GridLayout.LayoutParams());
+//            image.setImageResource(R.drawable.camera2);
+//            galleryGridLayout.addView(image);
+//        }
 
         galleryImageView.setVisibility(View.VISIBLE);
         galleryImageView.setImageResource(images.get(0).getSrc());
